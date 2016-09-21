ed3a95d09b95a84f5e2689b0818047f346cef993
Premkumar Masilamani <prem@smileprem.com>
2016-09-21 10:28:56 +0200

    Remove unused folders (#25)
    * Delete GAData.java
    
    * Delete getdata.bat
    
    * Delete getdata.sh
    
    * Delete template.txt
    
    * Delete .travis.yml
    
    * Delete Gemfile
    
    * Delete google0ea953fac4c1f63c.html
    
    * Update .gitignore
    
    * Delete vcs.xml
    
    * Delete run.sh

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source 'https://rubygems.org'
-gem 'github-pages'
-gem 'html-proofer'
-gem 'wdm', '>= 0.1.0' if Gem.win_platform?
-gem 'jekyll-import'
--- _backup/popular-posts/getdata.sh
+++
@@ -1,9 +0,0 @@
-
-#for line in books health social personal self-improvement challenges technology travelogue thought-experiments science
-#do
-#	grep -Ril "$line" /home/smileprem/git/prem82.github.io/_posts/*.md | cut -c56-| rev | cut -c4- | rev > temp/"$line".txt
-#done
-
-
-javac GAData.java
-java GAData
